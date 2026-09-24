23ef019fe5ec0a32889714f313cbf4633a8821bb
almamun528 <ayapths@gmail.com>
2026-09-24 21:07:40 +0600

    write database logic into repo

--- internal/repository/book_repository.go
+++ internal/repository/book_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/google/uuid"
 	"github.com/mamun-jsx/book-shop-backend-go-fiber/internal/models"
 	"gorm.io/gorm"
 )
@@ -16,4 +17,34 @@ func NewBookRepository(db *gorm.DB) *BookRepository {
 // create book to database
 func (r *BookRepository) Create(book *models.Book) error {
 	return r.db.Create(book).Error
+}
+
+// get all books
+func (r *BookRepository) FindAll() ([]models.Book, error) {
+	var books []models.Book
+	err := r.db.Find(&books).Error
+	return books, err
+}
+
+// get a single book by ID
+
+func (r *BookRepository) FindByID(id uuid.UUID) (*models.Book, error) {
+	var book models.Book
+	err := r.db.First(&book, "id = ?", id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &book, nil
+}
+
+// Update a single book into Database
+
+func (r *BookRepository) Update(book *models.Book) error {
+	return r.db.Save(book).Error
+}
+
+// delete a single book by id
+
+func (r *BookRepository) Delete(id uuid.UUID) error {
+	return r.db.Delete(&models.Book{}, "id = ?", id).Error
 }
