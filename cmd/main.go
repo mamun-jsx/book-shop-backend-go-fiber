1391396cc430a6ff81fd464e03e5867deadf97be
almamun528 <ayapths@gmail.com>
2026-09-24 20:34:30 +0600

    mount code into main file

--- cmd/main.go
+++ cmd/main.go
@@ -6,13 +6,17 @@ import (
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/mamun-jsx/book-shop-backend-go-fiber/internal/app"
+	"github.com/mamun-jsx/book-shop-backend-go-fiber/internal/router"
 )
 
 func main() {
 
 	application := app.BootstrapApp()
-
-	fiberApp:= fiber.New()
-
-	router.Setup
+	fiberApp := fiber.New()
+	router.SetupRoutes(fiberApp, application)
+	serverAddr := fmt.Sprintf(":%s", application.Config.APPport)
+	log.Printf("🚀 Server is running on http://localhost%s", serverAddr)
+	if err := fiberApp.Listen(serverAddr); err != nil {
+		log.Fatalf("server failed to run : %v", err)
+	}
 }
